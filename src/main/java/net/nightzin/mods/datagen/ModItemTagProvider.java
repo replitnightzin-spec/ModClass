57ba31c47c20277e7387be80456774d72c565fda
NIGHTZIN <hatsunemikutoro@gmail.com>
2025-09-14 18:02:21 -0300

    Aprendi datagen vulgo MELHOR COISA DE TODAS, volte aqui quando precisar

---
+++ src/main/java/net/nightzin/mods/datagen/ModItemTagProvider.java
@@ -0,0 +1,27 @@
+package net.nightzin.mods.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryWrapper;
+import net.nightzin.mods.item.ModItems;
+import net.nightzin.mods.util.ModTags;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.PINK_GARNET)
+                .add(ModItems.RAW_PINK_GARNET)
+                .add(Items.COAL)
+                .add(Items.STICK)
+                .add(Items.APPLE);
+    }
+}
